8acefe2f04c52edeea3453a5a4c82c4d2acdd515
King Robert Kehinde <robert.k@techie.com>
2024-01-22 15:45:30 +0000

    Header, Hero & Footer Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Heading } from "./(marketing)/_Components/heading";
+import { Heroes } from "./(marketing)/_Components/heroes";
+import { Footer } from "./(marketing)/_Components/footer";
 
 export default function Home() {
   return (
-   <>
-   <p className="text-red-500">Welcome To NextJS</p></>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+        <Footer />
+      </div>
+    </div>
   );
 }
